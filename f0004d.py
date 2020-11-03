bfff7b3ddeb1cb9f03d91091fab16f28626298e6
varaljai <71698297+varaljai@users.noreply.github.com>
2020-11-03 13:16:57 +0100

    Add files via upload

---
+++ f0004d.py
@@ -0,0 +1,5 @@
+magyar_mérföld = input('Hány magyar mérföldre van innen a sárkány? ')
+magyae_mérföld = int(magyar_mérföld)
+km = magyae_mérföld * 6.2321
+tengeri_mérföld / 1.4322
+print('Ez a táv', km, 'kilométert és', tengeri_mérföld,'tengeri_mérföldet jelent.') 
